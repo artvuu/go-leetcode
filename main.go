1f792fa54551f946ca53a36efc6ce39fece908ab
Devon Crebbin <devon@artvuu.group>
2023-10-02 17:46:20 +1100

    initial structure

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	name = "Gopher"
+)
+
+var programs = []any{
+	program1,
+}
 
 func main() {
-	fmt.Println("Hello, world.")
+	argsWithProg := os.Args
+	n, err := strconv.Atoi(argsWithProg[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	programs[n].(func())()
+
+	fmt.Println(argsWithProg[1])
+}
+
+func program1() {
+	fmt.Println("Hello World")
 }
